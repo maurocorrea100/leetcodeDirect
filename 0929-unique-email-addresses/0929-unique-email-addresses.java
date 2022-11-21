fdf75dd2cf307df13f7b37c804409437f7d591b0
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2022-11-21 12:48:49 -0600

    Time: 47 ms (27.68%), Space: 48.2 MB (59.22%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,18 +1,21 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        Set<String> set = new HashSet<>();
+               Set<String> set = new HashSet<>();
         for (String el: emails) {
-            String[] temp = el.split("@");
-            temp[0] = temp[0].concat("@");
-            // remove + portion
-            StringBuilder sb = new StringBuilder(temp[0]);
-              if(sb.indexOf("+") != -1){
-                temp[0] = sb.delete(sb.indexOf("+"), sb.indexOf("@")).toString();
+            String [] sArr = el.split("@");
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while(i<sArr[0].length()){
+                if(el.charAt(i) == '.'){
+                      i++;
+                    continue;
+                }else if(el.charAt(i) == '+'){
+                    break;
+                }
+                sb.append(el.charAt(i));
+                i++;
             }
-            // remove dots from local
-            temp[0] = temp[0].replaceAll("\\.","");
-           // System.out.println(temp[0]+temp[1]);
-            set.add(temp[0].concat(temp[1]));
+            set.add(sb.toString()+"@"+sArr[1]);
         }
         return set.size();
     }
