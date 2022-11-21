824d8a619a3a3cc0f350161753d9e23e32977442
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2022-11-21 12:52:13 -0600

    Time: 56 ms (23.33%), Space: 55.6 MB (5.25%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,21 +1,10 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-               Set<String> set = new HashSet<>();
+      Set<String> set = new HashSet<>();
         for (String el: emails) {
             String [] sArr = el.split("@");
-            StringBuilder sb = new StringBuilder();
-            int i = 0;
-            while(i<sArr[0].length()){
-                if(el.charAt(i) == '.'){
-                      i++;
-                    continue;
-                }else if(el.charAt(i) == '+'){
-                    break;
-                }
-                sb.append(el.charAt(i));
-                i++;
-            }
-            set.add(sb.toString()+"@"+sArr[1]);
+            String [] local = sArr[0].split("\\+");
+            set.add(local[0].replaceAll("\\.","") + "@" + sArr[1]);
         }
         return set.size();
     }
