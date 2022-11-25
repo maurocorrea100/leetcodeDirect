0a603ed2de00504e28d020700535d3009875e5f8
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2022-11-24 22:20:21 -0600

    Time: 16 ms (32.48%), Space: 42.7 MB (39.35%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+                if(!text.contains("b") || !text.contains("a")
+                || !text.contains("l")
+                || !text.contains("o")
+                || !text.contains("n")){
+            return 0;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < text.length(); i++){
+            boolean exists = text.charAt(i)=='b' || text.charAt(i)=='a' ||text.charAt(i)=='l' ||text.charAt(i)=='o' ||text.charAt(i)=='n';
+
+            if(exists){
+                if(map.containsKey(text.charAt(i))){
+                   map.put(text.charAt(i), map.get(text.charAt(i))+1);
+                }else{
+                    map.put(text.charAt(i),1);
+                }
+            }
+        }
+        
+        map.put('l', map.get('l')/2);
+        map.put('o', map.get('o')/2);
+
+        int minValue = map.values().stream().min(Integer::compare).get();
+
+        return minValue;
+    }
+}
