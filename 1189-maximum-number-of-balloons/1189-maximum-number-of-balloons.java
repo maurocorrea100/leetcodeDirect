e088fe864609816de05e05895952e7758e76a65e
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2022-11-24 22:25:22 -0600

    Time: 6 ms (70.30%), Space: 43.2 MB (16.21%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,26 +1,23 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-                if(!text.contains("b") || !text.contains("a")
-                || !text.contains("l")
-                || !text.contains("o")
-                || !text.contains("n")){
-            return 0;
-        }
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < text.length(); i++){
-            boolean exists = text.charAt(i)=='b' || text.charAt(i)=='a' ||text.charAt(i)=='l' ||text.charAt(i)=='o' ||text.charAt(i)=='n';
-
-            if(exists){
-                if(map.containsKey(text.charAt(i))){
-                   map.put(text.charAt(i), map.get(text.charAt(i))+1);
-                }else{
-                    map.put(text.charAt(i),1);
-                }
+     int b = 0, a = 0, l = 0, o = 0, n = 0;
+        
+        for(char ch : text.toCharArray()){
+            switch(ch){
+                case 'b': ++b;
+                        break;
+                case 'a': ++a;
+                        break;
+                case 'l': ++l;
+                        break;
+                case 'o': ++o;
+                        break;
+                case 'n': ++n;
+                        break;
+            
             }
         }
-        map.put('l', map.get('l')/2);
-        map.put('o', map.get('o')/2);
-
-        return map.values().stream().min(Integer::compare).get();
+        
+        return Math.min(Math.min(o/2, l/2), Math.min(Math.min(b, a), n));
     }
 }
