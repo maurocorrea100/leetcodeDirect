2493de706b0dd3d9583562a3bf8652721e1e9eaf
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2022-11-24 23:15:49 -0600

    Time: 4 ms (32.26%), Space: 51.4 MB (30.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,7 +1,5 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        if(nums.length==1 && nums[0]==0) return 1;
-        if(nums.length==1 && nums[0]==1) return 0;
         return (nums.length*(1 + nums.length))/2- Arrays.stream(nums).reduce(0, (a, b) -> a + b);
     }
 }
