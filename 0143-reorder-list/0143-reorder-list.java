b39dc9f267b0c43f0055fe9ea720f47473dca892
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2022-12-04 19:51:15 -0600

    Time: 1 ms (100.00%), Space: 45.2 MB (87.34%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,35 +10,41 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode slow = head, fast = head;
+             ListNode slow = head, fast = head;
         while(fast != null && fast.next != null  ){
             slow = slow.next;
             fast = fast.next.next;
         }
-    //    System.out.println("Slow value: " + slow.val);
 
-        Stack<ListNode> st = new Stack<>();
+
+//        System.out.println("Slow value: " + slow.val);
 
         ListNode temp = slow;
         slow = slow.next;
         temp.next = null;
 
+        ListNode prev = null;
+        //reverse linked list
         while(slow != null){
-            ListNode s = slow;
-            s = slow.next;
-            slow.next = null;
-            st.add(slow);
-            slow = s;
+            ListNode node = slow;
+            slow = slow.next;
+            node.next = prev;
+            prev = node;
         }
 
-        ListNode node = head;
-        ListNode tempNode = head;
-        while(!st.isEmpty()){
-            node = node.next;
-            tempNode.next = st.pop();
-            tempNode = tempNode.next;
-            tempNode.next = node;
-            tempNode = node;
+        ListNode head2 = prev;
+        ListNode curr2 = head2;
+        ListNode prev2 = head2;
+        ListNode curr1 = head;
+        ListNode prev1 = head;
+        while(curr2 != null){
+            curr1 = curr1.next;
+            curr2 = curr2.next;
+            prev2.next = null;
+            prev1.next = prev2;
+            prev2.next = curr1;
+            prev1 = curr1;
+            prev2 = curr2;
         }
 
     }
