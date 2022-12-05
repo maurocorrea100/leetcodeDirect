82dc8e24151562b02b37d77ad93b9d3a8c1f78d8
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2022-12-04 21:01:07 -0600

    Time: 1610 ms (5.01%), Space: 326.5 MB (5.00%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < s.length(); i++){
 
             for (int j = i+1; j < s.length(); j++){
-              //  if(pali.length()>j+1-i) break;
+             //   if(pali.length()>j+1-i) break;
                 if(pali.length() < (j-i+1) && isPlaindrome(s.substring(i,j+1))){
                     pali = s.substring(i,j+1);
                 }
