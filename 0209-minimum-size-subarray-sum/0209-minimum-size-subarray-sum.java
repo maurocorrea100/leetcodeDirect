d87756e6c6c3e8478a0c4176e2b0c7834b1a093a
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2022-12-19 16:20:45 -0600

    Time: 10 ms (18.74%), Space: 58.6 MB (9.46%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -5,17 +5,15 @@ class Solution {
 
         int minimal = Integer.MAX_VALUE;
 
-        int l=0, r= 0;
-        int sum = nums[l];
-        while(l < nums.length && r< nums.length){
-            if(sum>=target){
-                minimal = Math.min(minimal,r-l+1);
-                sum = sum - nums[l];
-                l++;
-            }else if(sum < target){
-                r++;
-                if(r >= nums.length) break;
-                sum = sum + nums[r];
+        int start=0;
+        int sum =0;
+        for(int end = 0; end < nums.length; end++){
+            sum = sum + nums[end];
+            
+            while(sum>=target){
+                minimal = Math.min(minimal, end-start+1);
+                sum = sum - nums[start];
+                start++;
             }
         }
         return minimal == Integer.MAX_VALUE ? 0 : minimal;
