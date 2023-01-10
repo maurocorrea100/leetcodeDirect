22e763810814544d2947db2331d4387328a90b64
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-01-10 13:55:36 -0600

    Time: 1 ms (100.00%), Space: 43.6 MB (55.91%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,19 +1,20 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        if(nums.length==1) return 0;
-        int sum1 = 0, sum2 = 0;
-        for (int num: nums) {
-            sum1+=num;
+        int totalSum = 0;
+        int leftSum = 0;
+        int res = -1;
+        
+        for(int i = 0; i < nums.length; i++){
+            totalSum = totalSum+nums[i];
         }
-        int lIndex = 0;
-        while(lIndex<nums.length){
-            sum1 = sum1 - nums[lIndex];
-            if(sum1 == sum2) return lIndex;
-            sum2 = sum2 + nums[lIndex];
-            lIndex++;
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            if(totalSum-nums[i] == 2 * leftSum) return i;
+            leftSum+=nums[i];
+            
         }
-
-      //  if(sum1 == sum2) return --lIndex;
-        return -1;
+        
+        return res;
     }
 }
