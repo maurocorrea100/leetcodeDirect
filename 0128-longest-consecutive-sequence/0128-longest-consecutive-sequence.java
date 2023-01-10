e7dea169a271c9366b3f04209e995bbc33941364
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-01-10 16:23:58 -0600

    Time: 24 ms (83.67%), Space: 61.3 MB (58.46%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -11,7 +11,7 @@ class Solution {
                 n++;
                 count++;
             }
-            res = res > count ? res : count;
+            if(res<count) res = count;
         }
         return res;
     }
