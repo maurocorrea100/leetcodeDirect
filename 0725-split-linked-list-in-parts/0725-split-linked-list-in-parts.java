ccb9c4fb2448afca7256a433b2df96c6030ce63b
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-01-23 21:48:53 -0600

    Time: 1 ms (42.05%), Space: 44.6 MB (7.09%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode[] splitListToParts(ListNode head, int k) {
-         ListNode temp = head;
+        ListNode temp = head;
         int count = 0;
         while(temp != null){
             count++;
@@ -21,54 +21,51 @@ class Solution {
             arr[0] = head;
             return arr;
         }
-            int rem = count%k;
-//            double part = Math.round(((double) count/ (double) k)*100.0)/100.0;
-            double part = Math.ceil((double) count/(double) k);
-            System.out.println(part);
-            System.out.println(rem);
-            int numberOfNonPart = k - rem;
-            int numberOfPart = k - numberOfNonPart;
-            ListNode curr = head;
-            int index = 0;
+        int rem = count%k;
+        double part = Math.ceil((double) count/(double) k);
+        
+        int numberOfPart = rem;
+        ListNode curr = head;
+        int index = 0;
 
-            while (numberOfPart>0){
-                int counter = 1;
-                ListNode temp2 = curr;
-                while(counter < part){
-                    temp2 = temp2.next;
-                    counter++;
-                }    
-                
+        while (numberOfPart>0){
+            int counter = 1;
+            ListNode temp2 = curr;
+            while(counter < part){
+                temp2 = temp2.next;
+                counter++;
+            }
+
+            ListNode temp3 = temp2.next;
+            temp2.next = null;
+            arr[index] = curr;
+            index++;
+            curr = temp3;
+            numberOfPart--;
+        }
+
+        while(index<arr.length){
+            int counter = 1;
+            if(rem == 0) counter = 0;
+            ListNode temp2 = curr;
+            while(counter < part-1){
+                temp2 = temp2.next;
+                counter++;
+            }
+
+            if(null!=temp2){
                 ListNode temp3 = temp2.next;
                 temp2.next = null;
                 arr[index] = curr;
                 index++;
                 curr = temp3;
-                numberOfPart--;
-            }
-            
-            while(index<arr.length){
-                int counter = 1;
-                if(rem == 0) counter = 0;
-                ListNode temp2 = curr;
-                while(counter < part-1){
-                    temp2 = temp2.next;
-                    counter++;
-                }
-
-                if(null!=temp2){
-                ListNode temp3 = temp2.next;
-                temp2.next = null;
+            }else{
                 arr[index] = curr;
                 index++;
-                curr = temp3; 
-                }else{
-                    arr[index] = curr;
-                    index++;
-                    break;
-                }
-                
+                break;
             }
+
+        }
         return arr;
     }
 }
