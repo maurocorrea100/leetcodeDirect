b1b049169298204a5d1ad7091a77a0b086982931
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-01-27 19:16:28 -0600

    Time: 2 ms (100.00%), Space: 47.7 MB (31.75%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,30 +10,40 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            Stack<Integer> s1 = new Stack<Integer>();
-        Stack<Integer> s2 = new Stack<Integer>();
+        int n1 = countNodes(l1), n2 = countNodes(l2);
+        ListNode result = null;
+        if(n1 > n2)
+            result = addTwoNumbers(l1, n1, l2, n2);
+        else
+            result = addTwoNumbers(l2, n2, l1, n1);
         
-        while(l1 != null) {
-            s1.push(l1.val);
-            l1 = l1.next;
-        };
-        while(l2 != null) {
-            s2.push(l2.val);
-            l2 = l2.next;
+        if(result != null && result.val > 9) {
+            result.val = result.val % 10;
+            return new ListNode(1, result);
         }
-        
-        int sum = 0;
-        ListNode list = new ListNode(0);
-        while (!s1.empty() || !s2.empty()) {
-            if (!s1.empty()) sum += s1.pop();
-            if (!s2.empty()) sum += s2.pop();
-            list.val = sum % 10;
-            ListNode head = new ListNode(sum / 10);
-            head.next = list;
-            list = head;
-            sum /= 10;
+        return result;
+    }
+    
+    int countNodes(ListNode node) {
+        int count = 0;
+        while(node != null) { count++; node = node.next; }
+        return count;
+    }
+    
+    public ListNode addTwoNumbers(ListNode l1, int n1, ListNode l2, int n2) {
+        if(n1 > 0) {
+            int val2 = 0;
+            if(n1 == n2) {
+                val2 = l2.val; l2 = l2.next; n2--;
+            }
+            ListNode result = addTwoNumbers(l1.next, n1 - 1, l2, n2);
+            int c = 0;
+            if(result != null && result.val > 9) {
+                c = 1;
+                result.val = result.val % 10;
+            }
+            return  new ListNode(l1.val + val2 + c, result);
         }
-        
-        return list.val == 0 ? list.next : list;
+        return null;
     }
 }
