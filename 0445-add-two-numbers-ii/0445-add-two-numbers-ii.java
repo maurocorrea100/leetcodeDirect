fe6db0bf51132c540dfc97ada4f2c03511fa862a
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-01-27 19:12:12 -0600

    Time: 5 ms (45.97%), Space: 42.6 MB (59.96%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,42 +10,47 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    Stack<ListNode> s1 = new Stack<>();
+        Stack<ListNode> s1 = new Stack<>();
         Stack<ListNode> s2 = new Stack<>();
-        
+
         ListNode temp = l1;
         fillStack(s1, temp);
 
         temp = l2;
         fillStack(s2,temp);
-        
+
         int carry = 0, sum = 0;
         Stack<ListNode> longSt = s1.size() >= s2.size() ? s1 : s2;
         Stack<ListNode> shortSt = s1.size() < s2.size() ? s1 : s2;
-        
+
         ListNode head = null;
         ListNode s = null;
         while(!shortSt.isEmpty()){
             head = longSt.pop();
             s = shortSt.pop();
             sum = head.val + s.val + carry;
-            carry = sum > 9 ? 1 : 0;
-            head.val = sum%10;
+            carry = getCarry(sum, head);
         }
-        
+
         while (!longSt.isEmpty()){
             head = longSt.pop();
             sum = head.val + carry;
-            carry = sum > 9 ? 1 : 0;
-            head.val = sum%10;
+            carry = getCarry(sum, head);
         }
-        
+
         if(carry == 1){
             temp = new ListNode(1);
             temp.next = head;
             head = temp;
         }
         return head;
+    }
+
+    private static int getCarry(int sum, ListNode head) {
+        int carry;
+        carry = sum > 9 ? 1 : 0;
+        head.val = sum % 10;
+        return carry;
     }
 
     private static void fillStack(Stack<ListNode> s1, ListNode temp) {
