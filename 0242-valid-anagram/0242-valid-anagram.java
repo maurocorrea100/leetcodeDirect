28af8930a0a40cdb35cbf2f743e01f20f6a061bc
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-02-17 19:14:08 -0600

    Time: 3 ms (81.52%), Space: 42.5 MB (65.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -8,11 +8,16 @@ class Solution {
         
         for(int i = 0; i < s.length(); i++){
             counter[s.charAt(i) - 'a']++;
-            counter[t.charAt(i) - 'a']--;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            int index = t.charAt(i) - 'a';
+            counter[index]--;
+            if(counter[index]<0) return false;
         }
         
         for(int element : counter){
-            if(element != 0)
+            if(element > 0)
                 return false;
         }
        
