e144c57fc4972248aa7616cfdbfcd4a8c8fb9e5e
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-02-17 20:06:17 -0600

    Time: 7 ms (42.24%), Space: 42.8 MB (42.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,28 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        if (s.length() != t.length())
-            return false;
-        
-        int[] counter = new int[26];
-        
-        for(int i = 0; i < s.length(); i++){
-            counter[s.charAt(i) - 'a']++;
+        ArrayList<Integer> list = new ArrayList<>(26);
+
+        for(int i = 0; i < 26; i++){
+            list.add(0);
         }
-        
+
         for(int i = 0; i < s.length(); i++){
+            int index = s.charAt(i) - 'a';
+            list.set(index, list.get(index)+1);
+        }
+
+        for(int i = 0; i < t.length(); i++){
             int index = t.charAt(i) - 'a';
-            counter[index]--;
-            if(counter[index]<0) return false;
+            list.set(index, list.get(index)-1);
+            if(list.get(index)<0) return false;
         }
-        
-        for(int element : counter){
-            if(element > 0)
-                return false;
+
+        for (int num : list) {
+            if(num > 0 || num < 0 ) return false;
         }
-       
         return true;
     }
-    
-    
 }
