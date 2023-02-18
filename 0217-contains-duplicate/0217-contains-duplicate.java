2be2b87b401a95264a6ca005809cb58510faa1dd
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-02-17 18:24:29 -0600

    Time: 11 ms (60.12%), Space: 54.9 MB (61.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,16 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-      
-        Arrays.sort(nums);
-        
-        for(int i = 1; i < nums.length; i++) {
-            
-            if(nums[i]==nums[i-1])
-                return true;
+        Set<Integer> list = new HashSet<>();
+
+        for (int num : nums) {
+            if(list.contains(num)) return true;
+            list.add(num);
         }
-        
-        return false;
+        return false; 
     }
 }
