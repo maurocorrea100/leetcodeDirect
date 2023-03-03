97f7807197561381fddbb6968ad1d720ee0055e4
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-03-03 12:23:33 -0600

    Time: 15 ms (35.07%), Space: 45.1 MB (47.10%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,27 +1,41 @@
 class Solution {
+    class MyObject implements Comparable<MyObject>{
+    public  Integer value;
+    public  Integer amount;
+
+    MyObject(int value, int amount){
+        this.value = value;
+        this.amount = amount;
+    }
+    @Override
+    public int compareTo(MyObject o) {
+        return this.amount.compareTo(o.amount);
+    }
+}
+    
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
+          Map<Integer,Integer> map = new HashMap<>();
 
         for (int n : nums) {
             if(map.containsKey(n)) map.put(n, map.get(n) + 1);
             else map.put(n,1);
         }
-
-        PriorityQueue<List<Integer>> queue = new PriorityQueue<>(k, Comparator.comparingInt(a -> a.get(1)));
+        
+        PriorityQueue<MyObject> q = new PriorityQueue<>(k);
 
         for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
-            if(queue.size()==k){
-                int val = queue.peek().get(1);
-                if(entry.getValue()>val){
-                    queue.poll();
-                    queue.offer(Arrays.asList(entry.getKey(), entry.getValue()));
+            if(q.size()==k){
+                int val1 = q.peek().amount;
+                if(entry.getValue()>val1){
+                    q.poll();
+                    q.offer(new MyObject(entry.getKey(), entry.getValue()));
                 }
-            }else queue.offer(Arrays.asList(entry.getKey(), entry.getValue()));
+            }else q.offer(new MyObject(entry.getKey(), entry.getValue()));
         }
 
         int[] arr = new int[k];
-        for (int i = 0; i < arr.length; i++){
-            arr[i] = queue.poll().get(0);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = q.poll().value;
         }
         return arr;
     }
