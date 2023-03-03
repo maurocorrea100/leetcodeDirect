d64ad69f6ac2b779830eca178cfea04c37afa04c
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-03-03 12:35:45 -0600

    Time: 3 ms (99.64%), Space: 45.8 MB (21.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,28 +2,44 @@ class Solution {
 
     
     public int[] topKFrequent(int[] nums, int k) {
-         	List<Integer>[] bucket = new List[nums.length + 1];
-	Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
-
-	for (int n : nums) {
-		frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
-	}
-
-	for (int key : frequencyMap.keySet()) {
-		int frequency = frequencyMap.get(key);
-		if (bucket[frequency] == null) {
-			bucket[frequency] = new ArrayList<>();
-		}
-		bucket[frequency].add(key);
-	}
-
-	List<Integer> res = new ArrayList<>();
-
-	for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
-		if (bucket[pos] != null) {
-			res.addAll(bucket[pos]);
-		}
-	}
-	return res.stream().mapToInt(i->i).toArray();
+                if (k == nums.length) {
+            return nums;
+        }
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int n : nums) {
+            if(n < min) min = n;
+            if(n > max) max = n;
+        }
+        
+        int[] aux = new int[max - min + 1];
+        for(int n : nums) {
+            aux[n - min]++;
+        }
+        
+        
+        List<Integer>[] bucket = new List[nums.length + 1];
+        int maxFreq = 0;
+        for(int i = 0; i < aux.length; i++) {
+            if(aux[i] > 0) {
+                if(bucket[aux[i]] == null) bucket[aux[i]] = new ArrayList<>();
+                bucket[aux[i]].add(i + min);
+                maxFreq = Math.max(maxFreq, aux[i]);
+            }
+        }
+        
+        int[] res = new int[k];
+        int idx = 0;
+        for(int i = maxFreq; i >= 0 && idx < k; i--) {
+            if(bucket[i] != null) {
+                for(int n : bucket[i]) {
+                    if(idx == k) break;
+                    res[idx++] = n;
+                }
+            }
+        }
+        
+        return res;
     }
 }
