42462e352084de8b8010eeba321a194b4e38fd49
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-03-06 20:27:10 -0600

    Time: 30 ms (94.29%), Space: 50.8 MB (16.69%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,10 +4,12 @@ class Solution {
         LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>();
 
         for (int i = 0; i < nums.length - 2; i++) {
+            if(nums[i]>0) break;
             if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
                 int target = 0 - nums[i];
                 int left = i + 1;
                 int right = nums.length - 1;
+                
 
                 while (left < right) {
                     if (nums[left] + nums[right] == target) {
