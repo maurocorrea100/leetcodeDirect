47c4d7ee0bb8f49f75b2c9fad6e41ac12e830af4
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-04-21 15:43:23 -0500

    Time: 170 ms (10.78%), Space: 52.4 MB (94.08%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -2,16 +2,21 @@ class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int[][] nums = new int[position.length][2];
         for(int i = 0; i < position.length; i++){
+            
             nums[i][0] = position[i];
             nums[i][1] = speed[i];
         }
         Arrays.sort(nums, Comparator.comparing(a -> -a[0]));
-        Stack<Double> st = new Stack<>();
+        double cur = 0;
+        int res = 0;
         for(int i = 0; i < nums.length; i++){
             double n = (target-nums[i][0])/(double)nums[i][1];
-            if(st.isEmpty() || st.peek()<n) st.push(n);
+            if(cur<n){
+                res++;
+                cur=n;
+            }
+            
         }
-        System.out.println(st);
-        return st.size();
+        return res;
     }
 }
