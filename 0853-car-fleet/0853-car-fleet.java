dcdfb5812412621a5917aa31bb80f9320ec9db8a
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-04-21 15:47:27 -0500

    Time: 190 ms (7.36%), Space: 57 MB (36.16%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,16 +1,16 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int[][] nums = new int[position.length][2];
+        double[][] nums = new double[position.length][2];
         for(int i = 0; i < position.length; i++){
-            
             nums[i][0] = position[i];
-            nums[i][1] = speed[i];
+            nums[i][1] = (target-position[i])/(double)speed[i];
         }
         Arrays.sort(nums, Comparator.comparing(a -> -a[0]));
         double cur = 0;
         int res = 0;
+        double n = 0.0;
         for(int i = 0; i < nums.length; i++){
-            double n = (target-nums[i][0])/(double)nums[i][1];
+            n = nums[i][1];
             if(cur<n){
                 res++;
                 cur=n;
