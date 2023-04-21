029b8c45bbed819127dc37af70e17b068aed3ff1
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2023-04-21 15:55:58 -0500

    Time: 31 ms (93.10%), Space: 62.2 MB (7.43%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,13 +1,19 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
          int res = 0;
-       double[] timeArr = new double[target];
+        int max = -1;
+        
+        for(int el : position){
+            max = Math.max(el,max);
+        }
+        
+       double[] timeArr = new double[max+1];
        for (int i = 0; i < position.length; i++)
         {
             timeArr[position[i]]= (double)(target - position[i]) / speed[i];
         }
         double prev = 0.0;
-        for (int i = target-1; i >=0 ; i--)
+        for (int i = max; i >=0 ; i--)
         {
             double cur = timeArr[i];
             if (cur > prev)
