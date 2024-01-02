45e883a9b49ad4adb0bed1f678789933e0ffb58e
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-01-01 23:38:04 -0600

    Time: 9 ms (8.05%), Space: 44.9 MB (18.64%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,23 +1,33 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        List<List<Integer>> lists = new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            List<Integer> tempList = new ArrayList<>();
-            boolean newRow = true;
-            for(List<Integer> l : lists){
-                if(!l.contains(nums[i])){
-                    l.add(nums[i]);
-                    newRow = false;
-                    break;
+         int a[]=new int[201];
+        for(int i:nums)
+        {
+            a[i]++;
+        }
+        List<List<Integer>> kk=new ArrayList<>();
+        while(true)
+        {
+            List<Integer> k=new ArrayList<>();
+            int x=0;
+            for(int i=0;i<201;i++)
+            {
+                if(a[i]>0)
+                {
+                    k.add(i);
+                    a[i]-=1;
+                }
+                else
+                {
+                    x++;
                 }
             }
-            if(newRow) {
-                tempList.add(nums[i]);
-                lists.add(tempList);
+            if(x==201)
+            {
+                break;
             }
+            kk.add(new ArrayList<>(k));
         }
-
-        return lists;
+        return kk;
     }
 }
