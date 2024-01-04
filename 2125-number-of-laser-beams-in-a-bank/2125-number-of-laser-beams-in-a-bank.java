4775c47632c877771c9aedce626f297368bc63cb
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-01-03 18:42:54 -0600

    Time: 12 ms (84.14%), Space: 45.6 MB (6.00%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,27 +1,15 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int res = 0;
-        for(int i = 0; i<bank.length; i++){
-            String flUp = bank[i];
-            int amountUp = count(flUp);
-            int j = i+1;
-            while(j < bank.length){
-                String flDown = bank[j];
-                int amountDown = count(flDown);
-                if(amountDown>0){
-                    res = res+ amountUp*amountDown;
-                    break;
-                }
-                j++;
-            }
+int ans = 0, prev = 0, count = 0;
+    for(String s: bank) {
+        count = 0;
+        for (int i = 0; i < s.length(); i++) 
+            if (s.charAt(i) == '1') count++;
+        if (count > 0) {
+            ans += prev * count;
+            prev = count;
         }
-        
-        return res;
     }
-    
-    int count(String s){
-        int sum = 0;
-        for(int i =0; i < s.length(); i++) if(s.charAt(i)=='1') sum++;
-        return sum;
+    return ans;
     }
 }
