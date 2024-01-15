952d8f3dc376c29f010d64732e37591cad1c3d8c
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-01-15 00:28:56 -0600

    Time: 244 ms (11.96%), Space: 109.1 MB (14.16%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-          List<Integer> winners = new ArrayList<>();
+        List<Integer> winners = new ArrayList<>();
         List<Integer> loosers1 = new ArrayList<>();
         List<List<Integer>> res = new ArrayList<>();
 
-       Map<Integer,ArrayList<Integer>> map = new TreeMap<>();
+       Map<Integer,int[]> map = new TreeMap<>();
 
        for(int[] pair : matches){
            int winner = pair[0], looser = pair[1];
            if(map.containsKey(winner)) {
-               map.get(winner).set(0,map.get(winner).get(0)+1);
-           }else map.put(winner,new ArrayList<>(Arrays.asList(1,0)));
+                map.get(winner)[0]++;
+           }else map.put(winner,new int[]{1,0});
 
            if(map.containsKey(looser)){
-               map.get(looser).set(1,map.get(looser).get(1)+1);
-           }else map.put(looser,new ArrayList<>(Arrays.asList(0,1)));
+               map.get(looser)[1]++;
+           }else map.put(looser,new int[]{0,1});
        }
 
         for (var entry : map.entrySet()){
-            int loosingScore = entry.getValue().get(1);
+            int loosingScore = entry.getValue()[1];
             if(loosingScore == 0) winners.add(entry.getKey());
             if(loosingScore == 1) loosers1.add(entry.getKey());
         }
