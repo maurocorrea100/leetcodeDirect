15daf59a05b0a3739159b230065937eb0edeac5c
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-02-05 22:05:58 -0600

    Time: 7 ms (57.75%), Space: 47.1 MB (85.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,6 +1,5 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-         List<List<String>> res = new ArrayList<>();
         HashMap<String,List<String>> map = new HashMap<>();
         for (String el:strs) {
             char[] arr = el.toCharArray();
@@ -8,8 +7,6 @@ class Solution {
             String sorted = new String(arr);
             if(map.putIfAbsent(sorted,new ArrayList<>(Arrays.asList(el))) !=null) map.get(sorted).add(el);
         }
-        
-        for(List<String> list : map.values()) res.add(list);
-        return res;
+        return new ArrayList<>(map.values());
     }
 }
