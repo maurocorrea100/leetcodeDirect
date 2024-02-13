d66190caef1706c9fd06c25ac85ea0b79fcfaf8d
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-02-12 20:06:36 -0600

    Time: 11 ms (11.79%), Space: 45.5 MB (7.44%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,15 +1,6 @@
 class Solution {
     public String firstPalindrome(String[] words) {
-         for(String el : words) if(isPoly(el)) return el;
+        for(String el : words) if(IntStream.range(0, el.length() / 2).allMatch(i -> el.charAt(i) == el.charAt(el.length() - i - 1))) return el;
         return "";
-    }
-    
-    public boolean isPoly(String el){
-        int l=0, r = el.length()-1;
-        while(l<r){
-           if(el.charAt(l) != el.charAt(r)) return false;
-           l++; r--;
-        }
-        return true;
     }
 }
