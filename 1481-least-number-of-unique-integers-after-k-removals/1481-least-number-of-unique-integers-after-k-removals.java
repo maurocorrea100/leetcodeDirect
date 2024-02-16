cf760ef4765db16b917534f647c662fe1df0cd3a
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-02-15 19:23:10 -0600

    Time: 66 ms (26.39%), Space: 56.6 MB (85.44%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,24 +1,11 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-         Map<Integer,Integer> map = new HashMap<>();
-        for(int el:arr) if(map.putIfAbsent(el,1)!=null) map.put(el,map.get(el)+1);
-        ArrayList<int[]> list = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> pair:map.entrySet()) list.add(new int []{pair.getKey(), pair.getValue()});
-        list.sort(Comparator.comparingInt(o -> o[1]));
-        int res = 0;
-        for(int i = 0; i < list.size(); i++){
-            int[] el = list.get(i);
-            int amount = el[1];
-            amount = amount - k;
-            if(amount>0){
-                res = list.size()-i;
-                break;
-            }else{
-                k = k - el[1];
-            }
-        }
-
-
-        return res;
+       Map<Integer,Integer> map = new HashMap<>();
+        for(int a:arr) map.put(a, 1 + map.getOrDefault(a, 0));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(map.values());
+        System.out.println(pq);
+        while (k > 0)
+            k -= pq.poll();
+        return k < 0 ? pq.size() + 1 : pq.size();
     }
 }
