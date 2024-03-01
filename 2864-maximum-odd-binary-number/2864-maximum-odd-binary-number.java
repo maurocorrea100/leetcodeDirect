bf8d95cac4086b82dc90813a244ae73fa42e0390
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-02-29 20:39:15 -0600

    Time: 4 ms (44.58%), Space: 43.4 MB (58.27%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -6,16 +6,12 @@ class Solution {
             int ind = str.indexOf("1");
             str.setCharAt(ind,'0');
             str.setCharAt(str.length()-1,'1');
-          //  return str.toString();
         }else{
-             str = new StringBuilder();
-            int count = 0;
-            for(int i = 0; i < s.length(); i++) if(s.charAt(i)=='1') count++;
-            for(int i = 0; i < count-1; i++){
-                nums[i] = 1;
-            }
-            nums[nums.length-1] = 1;
-            for(int el : nums) str.append(el);
+            str = new StringBuilder();
+           for(int i = 0; i < nums.length;i++) if(s.charAt(i)=='1') nums[i] = 1;
+           Arrays.sort(nums);
+           for(int i = nums.length-2; i >=0; i--) str.append(nums[i]);
+           str.append(1);
         }
           return str.toString();
     }
