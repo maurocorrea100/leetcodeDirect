0400c960c03b5126994e4eeb9f0b889ab1a734ff
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-02-29 20:30:41 -0600

    Time: 3 ms (52.17%), Space: 43.4 MB (50.14%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -1,22 +1,21 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
-         StringBuilder str = new StringBuilder(s);
+        int[] nums = new int[s.length()];
+        StringBuilder str = new StringBuilder(s);
         if(s.indexOf("1")==s.lastIndexOf("1")){
             int ind = str.indexOf("1");
             str.setCharAt(ind,'0');
             str.setCharAt(str.length()-1,'1');
+          //  return str.toString();
         }else{
-            str = new StringBuilder();
+             str = new StringBuilder();
             int count = 0;
             for(int i = 0; i < s.length(); i++) if(s.charAt(i)=='1') count++;
-            while(count>1){
-                str.append(1);
-                count--;
+            for(int i = 0; i < count-1; i++){
+                nums[i] = 1;
             }
-            while(str.length()<s.length()-1){
-                str.append(0);
-            }
-            str.append(1);
+            nums[nums.length-1] = 1;
+            for(int el : nums) str.append(el);
         }
           return str.toString();
     }
