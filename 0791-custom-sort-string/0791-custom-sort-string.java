4e60f7797ae9020d04807983d79e3ec28c2b84e4
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-03-11 15:33:33 -0500

    Time: 0 ms (100.00%), Space: 41.7 MB (27.20%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,20 +1,19 @@
 class Solution {
     public String customSortString(String S, String T) {
-           int[] count = new int[26];
-        for (char c : T.toCharArray()) { 
+        int[] count = new int[26];
+        for (char c : T.toCharArray()) 
             ++count[c - 'a'];  // count each char in T. 
-        }
+        
         StringBuilder sb = new StringBuilder();
-        for (char c : S.toCharArray()) {                            
-            while (count[c - 'a']-- > 0) { 
+        for (char c : S.toCharArray())                            
+            while (count[c - 'a']-- > 0) 
                 sb.append(c);  // sort chars both in T and S by the order of S.; 
-            }  
-        }
-        for (char c = 'a'; c <= 'z'; ++c) {
-            while (count[c - 'a']-- > 0) { 
-                sb.append(c); //chars in T but not in S.
-            }
-        }
+            
+        
+        for (char c = 'a'; c <= 'z'; ++c) 
+            while (count[c - 'a']-- > 0) 
+                sb.append(c);
+        
         return sb.toString();
     }
 }
