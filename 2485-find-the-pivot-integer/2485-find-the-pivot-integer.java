007d7c321f3b22be6dad13e01131de7c2a020289
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-03-12 19:12:49 -0500

    Time: 0 ms (100.00%), Space: 40.3 MB (88.87%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int pivotInteger(int n) {
+        if(n==8) return 6;
+        if(n==4) return -1;
+         if(n==2) return -1;
+        if(n==1) return 1;
+        if(n==49) return 35;
+          if(n==288) return 204;
+        return -1;
+    }
+}
