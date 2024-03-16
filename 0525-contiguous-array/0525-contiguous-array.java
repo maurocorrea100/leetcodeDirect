3ac45257ca0cd4d9ecafd36eddd2b0ddf82a6f4d
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-03-15 20:12:19 -0500

    Time: 32 ms (5.92%), Space: 55 MB (69.12%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -3,11 +3,12 @@ class Solution {
          int res = 0;
         Map<Integer,Integer> map = new HashMap<>();
         int prefixSum = 0;
-        for (int i = 0; i< nums.length; i++) nums[i] = nums[i] == 0 ? -1 : 1;
         
         for (int i = 0; i < nums.length; i++){
-            prefixSum = prefixSum + nums[i];
-            if(prefixSum==0) res = i+1;
+            prefixSum += nums[i] == 0 ? -1 :1;
+            if(prefixSum==0) {
+                if(res < i + 1) res = i+1;
+            }
             if(!map.containsKey(prefixSum)){
                 map.put(prefixSum,i);
             }
