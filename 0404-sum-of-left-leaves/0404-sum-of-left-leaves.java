c84e0203f493504f5c5cf724bc8ede800a4db8cf
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-03-15 21:07:05 -0500

    Time: 0 ms (100.00%), Space: 41.1 MB (87.19%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -15,22 +15,15 @@
  */
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-          int sum = 0;
+        int sum = 0;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         
         while(!queue.isEmpty()){
-            int size = queue.size();
-            while(size>0){
-                TreeNode node = queue.poll();
-                
-                if(node.left != null) {
-                    if(node.left.left==null && node.left.right==null) sum = sum + node.left.val;
-                    queue.add(node.left);
-                }
-                if(node.right != null) queue.add(node.right);
-                size--;
-            }
+            TreeNode node = queue.poll(); 
+            if(node.left != null && node.left.left==null && node.left.right==null) sum = sum + node.left.val;
+            if(node.left != null) queue.add(node.left);   
+            if(node.right != null) queue.add(node.right);
         }
         return sum;
     }
