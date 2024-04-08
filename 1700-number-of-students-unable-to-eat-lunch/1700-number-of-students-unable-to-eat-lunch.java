8fc9e240756abeab5579afcf65b915aca895a305
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-04-07 22:59:16 -0500

    Time: 2 ms (50.92%), Space: 41.4 MB (61.63%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,32 +1,32 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-         Stack<Integer> sandwichStack = new Stack<>();
-        Queue<Integer> studentQueue = new LinkedList<>();
-        // populate the stack and the queue
-        for (int i = 0; i < students.length; i++) {
-            sandwichStack.push(sandwiches[sandwiches.length-i-1]);
-            studentQueue.offer(students[i]);
+             Stack<Integer> st = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int countOnesSandwich = 0, countOnesStudent = 0;
+        for(int i = sandwiches.length-1; i >=0; i--){
+            st.push(sandwiches[i]);
+            if(sandwiches[i]==1) countOnesSandwich++;
         }
 
-        int couldntServe = 0;   // number of students that'll be left hungry
-        while (true) {
-            if (sandwichStack.peek() == studentQueue.peek()) {  // if types match
-                couldntServe = 0;       // served the current student
-                sandwichStack.pop();    // sandwich served
-                studentQueue.poll();    // student moves out of queue
-            } else {
-                couldntServe++;         // couldnt serve so increment count
-                studentQueue.offer(studentQueue.poll());    // move the student to the end of queue
-            }
-
-            // the unserved students will keep accumulating the the back of the queue
-            // until a situation would occur when we won't be able to serve them anymore due to type mismatch
-            // the queue will be left with only unservable students
-            if (couldntServe == studentQueue.size()) {  // that's when this condition would be hit
-                break;
-            }
+        for(int el : students) {
+            queue.add(el);
+            if(el==1) countOnesStudent++;
         }
+        if(countOnesStudent==countOnesSandwich) return 0;
 
-        return couldntServe;    // return the number of unserved students
+        int couldnotServe = 0;
+
+        while(true){
+            if(st.peek() == queue.peek()){
+                 couldnotServe=0;
+                st.pop();
+                queue.poll();
+            }else{
+                couldnotServe++;
+                queue.add(queue.poll());
+            }
+
+            if(couldnotServe== queue.size()) return couldnotServe;
+        }
 }
 }
