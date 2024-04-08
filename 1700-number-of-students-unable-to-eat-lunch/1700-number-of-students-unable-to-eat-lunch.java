c7114686eff415fe90f8406da0858c3fc6c58e29
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-04-07 22:50:54 -0500

    Time: 2 ms (50.92%), Space: 41.7 MB (29.87%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,19 +1,34 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-         int[] numberOfStudentsThatPreferEitherOneOrZero = new int[2];
-
-        for (int student : students) {
-            numberOfStudentsThatPreferEitherOneOrZero[student]++;
+       // We create a stack and the queue to simulate the serving process
+        Stack<Integer> sandwichStack = new Stack<>();
+        Queue<Integer> studentQueue = new LinkedList<>();
+        // populate the stack and the queue
+        for (int i = 0; i < students.length; i++) {
+            sandwichStack.push(sandwiches[sandwiches.length-i-1]);
+            studentQueue.offer(students[i]);
         }
 
-        for (int sandwich : sandwiches) {
-            if (numberOfStudentsThatPreferEitherOneOrZero[sandwich] == 0) {
-                return numberOfStudentsThatPreferEitherOneOrZero[1-sandwich];
+        int couldntServe = 0;   // number of students that'll be left hungry
+        while (true) {
+            if (sandwichStack.peek() == studentQueue.peek()) {  // if types match
+                couldntServe = 0;       // served the current student
+                sandwichStack.pop();    // sandwich served
+                studentQueue.poll();    // student moves out of queue
+            } else {
+                couldntServe++;         // couldnt serve so increment count
+                studentQueue.offer(studentQueue.poll());    // move the student to the end of queue
             }
-            numberOfStudentsThatPreferEitherOneOrZero[sandwich]--;
+
+            // the unserved students will keep accumulating the the back of the queue
+            // until a situation would occur when we won't be able to serve them anymore due to type mismatch
+            // the queue will be left with only unservable students
+            if (couldntServe == studentQueue.size()) {  // that's when this condition would be hit
+                break;
+            }
         }
 
-        return 0;
+        return couldntServe;    // return the number of unserved students
         
     }
 }
