cdff12641ce772fa3f430b8f32ca1e5c0e625f64
Ahmet Tachmuradov <32914686+ahmet422@users.noreply.github.com>
2024-04-18 16:19:00 -0500

    Time: 4 ms (99.57%), Space: 46.2 MB (5.01%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,16 +1,13 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-         int island = 0, neighbour = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
+        int island = 0, neighbour = 0;
+        for(int i = 0; i < grid.length; i++)
+            for(int j = 0; j < grid[i].length; j++)
                 if(grid[i][j] == 1){
                     island++;
                     if(j < grid[i].length-1 && grid[i][j+1] == 1) neighbour++;
                     if(i < grid.length-1 && grid[i+1][j] == 1) neighbour++;
                 }
-            }
-        }
-
         return 4*island - 2*neighbour;
     }
 }
